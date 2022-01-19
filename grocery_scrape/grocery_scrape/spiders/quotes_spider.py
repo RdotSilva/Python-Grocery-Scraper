0ba02391633f78e9b9025dbfa1a21b85dc77acc6
RdotSilva <RyandotSilva@gmail.com>
2022-01-19 21:06:11 +0000

    Add start_urls shortcut

--- grocery_scrape/grocery_scrape/spiders/quotes_spider.py
+++ grocery_scrape/grocery_scrape/spiders/quotes_spider.py
@@ -7,15 +7,23 @@ class QuotesSpider(scrapy.Spider):
     # Identifies the spider. It must be unique within a project, that is, you can’t set the same name for different Spiders
     name = "quotes"
 
-    def start_requests(self):
-        """must return an iterable of Requests (you can return a list of requests or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial requests."""
-        urls = [
-            "http://quotes.toscrape.com/page/1/",
-            "http://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    # Use start_urls instead of using the start_requests (think of this as a shortcut)
+    start_urls = [
+        "http://quotes.toscrape.com/page/1/",
+        "http://quotes.toscrape.com/page/2/",
+    ]
 
+    # Commented out because we are using start_urls above
+    # def start_requests(self):
+    #     """must return an iterable of Requests (you can return a list of requests or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial requests."""
+    #     urls = [
+    #         "http://quotes.toscrape.com/page/1/",
+    #         "http://quotes.toscrape.com/page/2/",
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    # Parse is scrapy's default callback
     def parse(self, response):
         """Method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it."""
         page = response.url.split("/")[-2]
