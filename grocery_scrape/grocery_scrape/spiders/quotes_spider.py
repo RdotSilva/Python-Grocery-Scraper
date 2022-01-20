5b72bfe98aa7a9843c1de27b05a0ef450d1d91ab
RdotSilva <RyandotSilva@gmail.com>
2022-01-20 20:22:17 +0000

    Add specific data to scrape

--- grocery_scrape/grocery_scrape/spiders/quotes_spider.py
+++ grocery_scrape/grocery_scrape/spiders/quotes_spider.py
@@ -26,8 +26,9 @@ class QuotesSpider(scrapy.Spider):
     # Parse is scrapy's default callback
     def parse(self, response):
         """Method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it."""
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        with open(filename, "wb") as f:
-            f.write(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
