86c42fa877ccaaeb8794bce80dca180aac8d6b30
RdotSilva <RyandotSilva@gmail.com>
2022-01-21 21:18:36 +0000

    Add parse function

--- grocery_scrape/grocery_scrape/spiders/grocery_spider.py
+++ grocery_scrape/grocery_scrape/spiders/grocery_spider.py
@@ -7,3 +7,10 @@ class GrocerySpider(scrapy.Spider):
     name = "grocery"
 
     start_urls = ["https://www.dunnesstoresgrocery.com/"]
+
+    def parse(self, response):
+        for item in response.css('div[data-testid="productListingShoppingRuleWrapper"]').extract() 
+            yield {
+                'text': item.css('div.text::text').get(),
+
+            }
